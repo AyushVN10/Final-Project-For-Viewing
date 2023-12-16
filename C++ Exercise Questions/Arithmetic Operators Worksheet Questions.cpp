4bb1315d233dea11d77441b5f25d16b2eb9ab078
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 13:59:37 -0500

    Create Arithmetic Operators Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Arithmetic Operators Worksheet Questions.cpp
@@ -0,0 +1,31 @@
+/* 
+Questions 1.3.1: Code Analysis and Output Prediction
+
+Consider the following code snippet:
+
+int x = 7, y = 3, z;
+z = x % y + 4 * (x - y);
+std::cout << "Result: " << z;
+
+a. Explain the order of execution for the arithmetic operations in the expression z = x % y + 4 * (x - y).
+
+b. Predict and write the output that will be displayed on the console when this code is executed.
+
+
+
+Questions 1.3.2: Variable Modification and Output
+
+In the main function, initialize two variables, length, and width, with values 8 and 5, respectively. 
+Calculate and assign the area of a rectangle to a variable named area using the formula area = length * width. 
+Additionally, calculate and assign the remainder of length divided by width to a variable named remainder using the modulus operator. 
+Then, output both the area and remainder with informative messages using std::court.
+
+
+
+Questions 1.3.3: Interactive Calculation
+
+Write a program that takes user input for the base and height of a triangle. 
+Calculate and output the area of the triangle using the formula area = 0.5 * base * height. 
+Ensure that the program prompts the user for input, and the output is formatted with a clear message.
+Assume that the user will always enter valid numerical values.
+*/
