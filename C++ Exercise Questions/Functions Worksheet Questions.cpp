908364ed71c4ef819c01f9f72392f1d4ea346b6a
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:04:48 -0500

    Create Functions Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Functions Worksheet Questions.cpp
@@ -0,0 +1,25 @@
+/*
+Question 3.3.1: Virtual Pet Adoption Center
+
+You are creating a virtual pet adoption center where users can adopt unique virtual pets. 
+Design a C++ program with a function named adoptVirtualPet that takes two parameters: petName (string) and petType (string). 
+The function should simulate the adoption process and print a message like: "Congratulations! You've adopted a [petType] named [petName]". 
+In your main function, demonstrate calling this function with various virtual pet names and types to showcase the versatility of functions in managing different scenarios.
+
+
+
+Question 3.3.2: Palindrome Check
+
+Develop a C++ function named isDynamicPalindrome that takes a string as a parameter and returns a boolean indicating whether the string is a dynamic palindrome. 
+In a dynamic palindrome, characters are symmetrically swapped from the center outwards. 
+For example, "abcdeedcba" is a dynamic palindrome. Test your function with different strings in your main function.
+
+
+
+Question 3.3.3: Gravitational Pull Calculator
+
+Implement a C++ function named calculateGravitationalPull that takes two parameters: mass (m in kg) and distance from the center (r in m). 
+Use these parameters in a formula to calculate gravitational pull. 
+Create a program that utilizes this function to calculate the gravitational pull for a given mass at varying distances from 1 m to 50 m, in increments of 5. 
+Allow the user to input the mass for experimentation.
+*/
