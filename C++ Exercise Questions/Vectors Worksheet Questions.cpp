a18f00d536b8759cb75d37cfcbb2f421a4ed14e8
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:11:06 -0500

    Create Vectors Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Vectors Worksheet Questions.cpp
@@ -0,0 +1,23 @@
+/*
+Question 4.2.1: Attraction Galaxy Adventure
+
+You're the lead programmer at the "VectorVille Theme Park" project. 
+Design a C++ program called "AttractionGalaxy Adventure" that uses vectors to manage attractions in different zones. 
+Create functions like "AddNewAttraction," "RemoveAttraction," and "DisplayZoneAttractions" to ensure a dynamic and fun experience for park visitors.
+
+
+
+Question 4.2.2: Vector Racer Showdown
+
+Welcome to the "VectorRacer Showdown" game! As a game developer, write a C++ function called "VectorRacerWinner" that determines the winner of a race based on vectorized race progress. 
+Ensure that the game accommodates dynamic race lengths and provides a thrilling experience for players as they compete in the VectorRacer championship.
+
+
+
+Question 4.2.3: Vector Voyager Quest
+
+In the realm of programming adventures, you find yourself on the "VectorVoyager Quest." 
+Your task is to design a C++ program that utilizes all the functionalities of vectors discussed in the notes. 
+Create a vector called "ExplorerVector" to represent your journey. Implement features like dynamically resizing the vector during exploration, and updating elements to mark discovered landmarks. 
+Showcase the versatility of vectors as you navigate through uncharted territories in the VectorVoyager world.
+*/
