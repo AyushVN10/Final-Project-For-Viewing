c503e107f158614ce16d5a7d819cd66b256da856
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:05:49 -0500

    Create Multidimensional Arrays Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Multidimensional Arrays Worksheet Questions.cpp
@@ -0,0 +1,27 @@
+/*
+Question 4.4.1: Sudoku Solver Extravaganza
+
+Imagine you've been recruited by the "Sudoku Superstars" organization to create a mind-bending Sudoku solver in C++.
+Your task is to implement a function called solveSudoku that takes a 2D vector representing the Sudoku board as input and returns the solved board. 
+Use your knowledge of multidimensional arrays to access and modify individual elements. 
+Additionally, demonstrate the use of nested loops to iterate through the entire board and check for a valid solution.
+
+
+
+Question 4.4.2: Pixel Power-Up Challenge
+
+You've landed a gig at the "Pixel Power-Up" game studio, and they need your C++ wizardry to implement a 3x3 convolution filter for a grayscale image. 
+Create a function named applyConvolutionFilter that takes a 2D array representing the image and applies a predefined filter to enhance pixel intensity. 
+Use your multidimensional array skills to access and update pixel values, and showcase the power of nested loops for the convolution process.
+
+
+
+Question 4.4.3: The Maze Navigator
+
+Imagine you're in a whimsical maze filled with twists and turns, and your goal is to navigate through it using C++. 
+Your position in the maze is represented as a 2D array, where each cell is either an open path ('O') or a wall ('#'). 
+You start at the top-left corner (maze[0][0]) and need to reach the bottom-right corner (maze[m-1][n-1]).
+
+Your task is to write a C++ function called navigateMaze that takes the maze as input and returns a boolean indicating whether it's possible to navigate to the destination. 
+You can move only down or right, and you cannot move through walls.
+*/
