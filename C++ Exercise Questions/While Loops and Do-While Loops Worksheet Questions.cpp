55a6b80c1551da7564cc8177238808620303fc98
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:11:51 -0500

    Create While Loops and Do-While Loops Worksheet Questions.cpp

---
+++ C++ Exercise Questions/While Loops and Do-While Loops Worksheet Questions.cpp
@@ -0,0 +1,33 @@
+/*
+Question 3.1.1: Dynamic Counter
+
+In your C++ program, create a while loop that dynamically adjusts its condition based on user input. 
+Prompt the user to enter a positive integer value, and then utilize a while loop to print the squares of numbers from 1 to the entered value. 
+Implement a break statement to terminate the loop if the squared value exceeds 50. 
+Ensure that the loop adheres to the principles of proper initialization, condition, and iteration.
+
+
+
+Question 3.1.2: The Dungeon Explorer
+
+Imagine creating a simple text-based dungeon explorer game.
+Set up a scenario where the player finds themselves in a mysterious dungeon with multiple rooms. Inside each room, the player encounters a challenge represented by a random number (e.g., 1 to 5). 
+Prompt the player to enter their chosen difficulty level (1 to 5) before entering the dungeon.
+Implement a do-while loop that continues until the player decides to exit voluntarily. 
+In each iteration of the loop, generate a random challenge difficulty for the current room. 
+The player must then input their skill level, and the program should compare the player's skill level with the challenge difficulty.
+If the player's skill level is greater or equal to the challenge difficulty, print a success message and prompt them to proceed to the next room.
+If not, inform the player of their defeat and ask if they want to try the same room again. Use the "do-while" loop to ensure the player gets at least one chance to overcome each challenge.
+Include an option for the player to exit the dungeon voluntarily, breaking out of the loop. Use the "else" statement to print a victory message when the player completes all the rooms.
+
+
+
+Question 3.1.3: Countdown to Ignition
+
+Develop a C++ program to simulate a countdown for a rocket launch. 
+Initialize a variable to represent the countdown value (e.g., 10). 
+Use a while loop to print the current countdown value in each iteration. 
+Implement a "break" statement to exit the loop when the countdown reaches zero. 
+Additionally, add an "else" statement to print a message signaling the ignition of the rocket after the loop concludes. 
+Ensure the loop stops if the countdown somehow becomes negative to prevent indefinite execution.
+*/
