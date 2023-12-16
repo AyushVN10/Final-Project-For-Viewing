4d9eb05049e1a58f3e41fc3ee0be1e649b86b7fe
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:08:58 -0500

    Create Standard Library Algorithms Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Standard Library Algorithms Worksheet Questions.cpp
@@ -0,0 +1,36 @@
+/*
+Question 4.5.1: Sorting and Searching in Vectors
+
+You have a vector named numbers with the following elements: {5, 2, 8, 1, 7, 3, 6, 4}. Using the knowledge gained from the notes on C++ Standard Library Algorithms, perform the following tasks:
+a) Sort the vector in ascending order.
+b) Search for the element '7' in the sorted vector using the std::binary_search algorithm. Print whether the element is found or not.
+c) Reverse the vector.
+d) Find the minimum and maximum elements in the reversed vector using the std::min_element and std::max_element algorithms.
+
+
+
+Question 4.5.2: Matrix Operations
+
+Consider the 2D vector (matrix) named "matrix":
+{
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+}
+Perform the following operations:
+a) Print the original matrix.
+b) Modify the element at row 0, column 1 to have the value '10'.
+c) Add a new row to the matrix with elements {11, 12, 13}.
+d) Iterate through the modified matrix and print each element.
+
+
+
+Question 4.5.3: Manipulating Sets
+
+You have a set named mySet with the elements {5, 2, 8, 3}. Use the knowledge from the notes on sets and algorithms:
+a) Insert the elements 6 and 1 into the set.
+b) Check if the element '4' is present in the set. Print the result.
+c) Remove the element '2' from the set.
+d) Display the elements of the set after the modifications in ascending order.
+Ensure that the questions involve the application of algorithms mentioned in the C++ Standard Library and the manipulation of collections discussed in the notes.
+*/
