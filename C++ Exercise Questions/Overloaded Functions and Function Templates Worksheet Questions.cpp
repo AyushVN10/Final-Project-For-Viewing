b055a7e3e404ba3b4375b145a1af5fbc08aa8204
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:07:24 -0500

    Create Overloaded Functions and Function Templates Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Overloaded Functions and Function Templates Worksheet Questions.cpp
@@ -0,0 +1,25 @@
+/*
+Question 3.4.1: Shape Sorcery
+
+Imagine you're a wizard in the magical land of C++. 
+Create two overloaded functions named "castSpell" – one that takes an integer representing the strength of a fire spell and another that takes a double representing the magnitude of an ice spell. 
+Demonstrate the magical prowess of these functions in the main program by casting a fire spell with strength 8 and an ice spell with magnitude 5.3. Display the mystical results of your sorcery.
+
+
+
+Question 3.4.2: Numeric Harmony
+
+Compose a symphony of numbers in the realm of C++ using the function template "orchestrate." 
+This template should take two musical notes (numeric values) of any type and blend them into a harmonious average. 
+In the main program, conduct the orchestration by finding the harmonic average of two integers (e.g., 7 and 12) and two doubles (e.g., 2.5 and 4.8).
+Let the console echo the sweet melody of your numeric harmony.
+
+
+
+Question 3.4.3: Comparison Carnival
+
+Join the excitement at the C++ Comparison Carnival! 
+Create a function template named "rollercoaster" that embraces the thrill of finding the maximum among two exhilarating values of any comparable type. 
+Ride the rollercoaster in the main program by determining the peak experience among two integers (e.g., 18 and 25), two doubles (e.g., 3.7 and 9.1), and two characters (e.g., 'C' and 'Z'). 
+Share the adrenaline-pumping results with the adventurous souls attending the carnival.
+*/
