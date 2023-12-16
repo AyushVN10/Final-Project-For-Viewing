20881240e62e1240819a080e982ba8e0c94ffb5e
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:04:15 -0500

    Create For Loops and Nested Loops Worksheet Questions.cpp

---
+++ C++ Exercise Questions/For Loops and Nested Loops Worksheet Questions.cpp
@@ -0,0 +1,25 @@
+/*
+Question 3.2.1: The Loop Maze Code Challenge
+
+You find yourself trapped in a mysterious Loop Maze and must write a C++ program to escape using nested for loops. 
+Code your way through the maze by implementing nested loops. 
+In this coding quest, each iteration of the outer loop should represent a move in the maze, and the inner loop's magic helps you overcome obstacles. 
+Utilize the "break" statement to exit the loops when you successfully navigate the maze and reach freedom.
+
+
+
+Question 3.2.2: Kingdom of For Loop Adventures
+
+Welcome to the enchanted Kingdom of For Loop Adventures! Craft a C++ program using for loops and nested loops to create a captivating menu-driven quest. 
+Imagine a world where adventurers embark on epic journeys, and each iteration of the loops unfolds a new chapter in their quests.
+Harness the power of for loops to guide heroes through the kingdom, and use the "break" statement to gracefully conclude their adventures.
+
+
+
+Question 3.2.3: Pattern Party with For Loops
+
+It's time to throw a Pattern Party using for loops in C++! Imagine you're organizing the coolest pattern-based extravaganza. 
+Describe the pattern you want to showcase and provide the code that brings it to life with a for loop. 
+Share a creative scenario where each loop iteration contributes to the vibrant atmosphere of the party. 
+Wrap it up with the "else" keyword, signaling the end of a fantastic coding fiesta!
+*/
