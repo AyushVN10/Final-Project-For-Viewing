d34a06eda268604d1a7f402b6fea2b5fb0192168
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:01:58 -0500

    Create Conditional Statements Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Conditional Statements Worksheet Questions.cpp
@@ -0,0 +1,45 @@
+/* 
+Question 2.1.1: Artistic Code Blocks
+
+Imagine you are designing a program to create ASCII art based on user input. Use C++ 'if' statements to achieve the following:
+
+a) If the user enters 'circle,' print a circle ASCII art.
+b) If the user enters 'square,' print a square ASCII art.
+c) If the user enters anything else, display a message saying, "Sorry, that shape is not supported."
+
+
+
+Question 2.1.2: C++ Movie Ticket Pricing
+
+You are crafting a program to determine movie ticket prices based on different age groups. The pricing scheme is as follows:
+
+Age Range	Ticket Price ($)
+0 – 12	5.00
+13 – 17	7.50
+18 – 59	10.00
+60 or more	8.00
+Develop a C++ program named movie_ticket_pricing that accomplishes the following tasks:
+
+Prompt the user to enter their age.
+Employ conditional statements to calculate and display the appropriate ticket price based on their age.
+Output the determined ticket price for the user to see.
+
+
+
+Questions 2.1.3: Adventure Game Decision Maker
+
+You are designing an interactive text-based adventure game in C++. 
+As the protagonist traverses through different scenarios, the game presents decision points.
+Each decision influences the outcome of the adventure. Use the concepts from the lesson to create a decision-making program:
+
+a) Define two variables representing the protagonist's health and courage.
+b) Set up an "if" statement to check the health level:
+  - If health is above 75, output a message encouraging the protagonist to face challenges bravely.
+  - If health is between 50 and 75, provide a warning about potential dangers ahead.
+  - If health is below 50, advise the protagonist to prioritize healing before facing further challenges.
+c) Incorporate an "else if" statement based on the courage level:
+  - If courage is high, encourage the protagonist to take bold actions.
+  - If courage is moderate, suggest considering risks before making decisions.
+  - If courage is low, recommend a cautious approach to avoid potential pitfalls.
+d) Output messages based on both health and courage conditions to guide the protagonist in their adventure.
+*/
