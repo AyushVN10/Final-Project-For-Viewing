baa8fb0d72be9a0190a927934bf868fd1675a4c8
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:09:59 -0500

    Create Ternary Operators and Switch Statements Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Ternary Operators and Switch Statements Worksheet Questions.cpp
@@ -0,0 +1,24 @@
+/*
+Question 2.3.1: Ternary Twist
+
+You are tasked with implementing a program that determines whether a given year is a leap year or not. 
+Create a program that uses the ternary operator to assign the result to a string variable named leapYearStatus. 
+Explain the conditions for a leap year and demonstrate how the ternary operator simplifies the code compared to an if-else statement.
+
+
+
+Question 2.3.2: Switch Showdown
+
+Imagine you are developing a simple calculator program in C++ that performs basic arithmetic operations based on user input. 
+Design the program using a switch statement to handle the different operations (addition, subtraction, multiplication, and division). 
+Discuss the advantages of using a switch statement in this context, and provide a clear example illustrating the use of the switch statement for calculator operations.
+
+
+
+Question 2.3.3: Day Decoder
+
+Create a C++ program that prompts the user to enter a day of the week as an integer (1-7). 
+Use a switch statement to determine the day's name and whether it is a weekday or weekend day. 
+Additionally, employ the ternary operator to decide and display a message indicating if the day's number is even or odd.
+Provide comments in your code to explain the logic behind each decision and ensure adherence to the concepts covered in the provided notes.
+*/
