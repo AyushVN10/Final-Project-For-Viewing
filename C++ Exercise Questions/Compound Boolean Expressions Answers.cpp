11b1aae8d236033c0a642322961b07172aaa14bf
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:00:57 -0500

    Create Compound Boolean Expressions Answers.cpp

---
+++ C++ Exercise Questions/Compound Boolean Expressions Answers.cpp
@@ -0,0 +1,45 @@
+/*
+Question 2.4.1: Logical Wizardry
+
+Given the variables, int temperature = 28 and bool isSummer = true, create a compound boolean expression using logical operators.
+The condition should output "Perfect weather" if the temperature is between 20 and 30 (inclusive) and it's summer. 
+Otherwise, it should output "Consider indoor activities." Write the C++ code snippet for this scenario.
+
+
+
+Question 2.4.2: Scholarship Eligibility
+
+You are designing a scholarship eligibility check for a university. The eligibility criteria are as follows:
+
+The applicant must be either a current high school student (represented by the boolean variable bool isHighSchoolStudent) or a college freshman (bool isCollegeFreshman).
+The applicant's age must be between 17 and 25 (inclusive).
+The applicant must not have any previous scholarship (represented by the boolean variable bool hasPreviousScholarship).
+Write a C++ if-statement using compound boolean expressions to determine if an applicant is eligible for the scholarship. 
+If the applicant is eligible, output "Congratulations! You qualify for the scholarship," otherwise output "Sorry, you do not meet the eligibility criteria." Use appropriate variables and values.
+
+
+
+Question 2.4.3: Gym Membership Eligibility
+
+The local Pokemon Gym offers different membership fees based on age and the number of badges trainers have earned. 
+Write a C++ program to determine the eligibility and membership fee for trainers applying for gym membership. Here are the conditions:
+
+Trainers must be at least 14 years old to be eligible.
+Trainers are rated based on the number of badges they have earned. Use the codes B, S, and G for Bronze, Silver, and Gold badges, respectively.
+Membership fees are determined as follows:
+- For trainers aged 14-25 (inclusive):
+  - Bronze badge holders pay $3.50.
+  - Silver badge holders pay $2.50.
+  - Gold badge holders pay $1.50.
+- For trainers aged 26-40 (inclusive):
+  - Bronze badge holders pay $2.50.
+  - Silver badge holders pay $1.75.
+  - Gold badge holders pay $1.00.
+- For trainers aged 41 or older:
+  - Bronze, Silver, and Gold badge holders pay $1.00.
+Write a C++ program that takes the trainer's age and badge code as input and outputs the eligibility status and the corresponding membership fee.
+
+Ensure to check the eligibility before calculating the membership fee. If a trainer is not eligible, output a message indicating ineligibility.
+
+Use compound boolean expressions to implement the conditions for determining eligibility and membership fees.
+*/
