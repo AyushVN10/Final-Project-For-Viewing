164cb1c85ebfcf3b00698f7bf0ef5692e01df2f0
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:08:22 -0500

    Create Sets and Maps Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Sets and Maps Worksheet Questions.cpp
@@ -0,0 +1,26 @@
+/*
+Question 4.3.1: Pirate's Puzzle Plunder
+
+As the official treasure map designer for a pirate crew, create a map called treasureMap where each location is marked with a unique clue. 
+Add clues for places like "Skull Island," "Mermaid Lagoon," and "Cursed Cove." Incorporate a new location, "Hidden Harbor," into the map. 
+Arrange the clues in alphabetical order and unveil the secrets hidden within, especially if "Skull Island" is a lurking treasure trove.
+
+
+
+Question 4.3.2: Magical Menagerie Mayhem
+
+You are orchestrating a magical creature convention, and chaos ensues! 
+Implement a set named creatureAttendees to manage the attendee IDs for creatures like "Phoenix," "Dragon," "Mermaid," and "Centaur."
+Introduce a new magical creature, "Gryphon," to the mix. 
+Calculate the total number of magical creatures attending and ensure no magical creature has gate-crashed the party.
+
+
+
+Question 4.3.3: Cosmic Cafe Reservations
+
+You are managing reservations for a futuristic Cosmic Cafe, where extraterrestrial beings gather for cosmic delicacies. Utilize both sets and maps to organize the reservation system:
+a) Alien Set: Create a set called cosmicVisitors to track the alien species arriving for a dining experience. Include species like "Lunar Lepus," "Nebula Nymph," and "Stellar Squid."
+b) Dining Preferences Map: Develop a map named diningPreferences to capture the dining preferences of each alien species. Assign preferences such as "Zero-G Noodles" for "Lunar Lepus," "Galactic Gushers" for "Nebula Nymph," and "Celestial Sushi" for "Stellar Squid."
+c) New Reservation: A new alien species, "Quantum Quokka," just made a reservation. Add "Quantum Quokka" to the set and assign its dining preference as "Quantum Quinoa."
+d) Print Confirmation: Display the finalized set of cosmic visitors and their corresponding dining preferences.
+*/
