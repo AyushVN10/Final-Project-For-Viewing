3ab1cfb036704dc1221322a7aead244afbe87654
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:00:35 -0500

    Create Arrays and Memory Allocation Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Arrays and Memory Allocation Worksheet Questions.cpp
@@ -0,0 +1,27 @@
+/*
+Questions 4.1.1: Element Swap
+
+Description: You are working on an image processing algorithm, and you need to perform a specific operation on an array of pixel values. 
+Implement a function swapAdjacentElements that takes an integer array representing pixel values. The function should swap each element with its adjacent element. 
+Ensure that you don't go out of bounds while swapping elements. Display the modified array after the swapping operation using a loop. 
+Additionally, use the sizeof() function to determine the size of the array dynamically.
+
+
+
+Questions 4.1.2: Dynamic Initialization
+
+Description: Assume you are developing a program where the size of an array is not known at compile time but is determined during runtime. 
+Implement a function initializeDynamicArray that takes an integer argument representing the size and dynamically allocates memory for an integer array of that size. 
+Ensure to initialize the array elements with consecutive integers starting from 1. 
+Finally, display the elements of the dynamically allocated array using a loop. Remember to deallocate the memory at the end of the program.
+
+
+
+Questions 4.1.3: Positive Transformation Array
+
+Description: Create a beginner-friendly C++ program that transforms an array of integers into a positively charged array. Follow these steps:
+  - Prompt the user to enter non-zero integers one by one and store them in an array. Stop accepting data when the user enters zero.
+  - Print out the contents of the array.
+  - Iterate through the array and transform all negative integers into their positive counterparts (e.g., change -5 to 5).
+  - Print out the array contents again, ensuring that all integers are positive.
+*/
