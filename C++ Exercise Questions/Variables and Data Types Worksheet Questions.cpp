624d143616c2271a91859ef35178bdbcbc11efc8
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:10:33 -0500

    Create Variables and Data Types Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Variables and Data Types Worksheet Questions.cpp
@@ -0,0 +1,34 @@
+/*
+Questions 1.1.1: Variable Challenge
+
+Imagine you're working on a program to manage a school's information system. Declare variables to store the following information using appropriate data types:
+
+Number of students in a class
+Average height of students (in meters)
+The first letter of the teacher's last name
+Whether the class has a project (true/false)
+The name of the school (use a std::string)
+Make sure that you follow the rules for naming variables in C++.
+
+
+
+Question 1.1.2: Numeric Puzzle
+
+Create a C++ program to solve a numeric puzzle. Declare three variables a, b, and c with initial values 5, 2, and 3, respectively. 
+Use these variables to calculate and print the result of the expression a * b + c / a - b. 
+Ensure that each step of the calculation has comments describing what is happening.
+
+
+
+Question 1.1.3: Coding Results
+
+In a prestigious coding competition, participants are awarded different grades based on their performance. 
+Your task is to implement a C++ program that uses namespaces to represent the grades. Create a variable named "grade" and assign it values based on the following criteria:
+
+If a participant secured the first position, set the grade to reflect a perfect average of 100.
+If a participant secured the second position, set the grade to reflect an outstanding average of 98.
+If a participant secured the third position, set the grade to reflect an excellent average of 95.
+For all other positions, set the grade to a default value of 90.
+
+Now, print out the grade using the appropriate namespace based on the participant's position. Make sure to showcase the use of namespaces to avoid any conflicts in variable names.
+*/
