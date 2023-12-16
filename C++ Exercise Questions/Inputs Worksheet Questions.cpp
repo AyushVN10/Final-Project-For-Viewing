0be373450b6c937c5532cb9357d360facc538411
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:05:19 -0500

    Create Inputs Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Inputs Worksheet Questions.cpp
@@ -0,0 +1,44 @@
+/*
+Questions 1.2.1: Mystery Output
+
+Imagine you are a detective exploring the mysterious world of C++ output! 
+Your mission is to decipher what will be printed to the console when the following C++ code, named "MysteryOutput.cpp," is executed. 
+Analyze the code based on the knowledge you've gained from the C++ lessons. What will be the output for each variable?
+
+#include <iostream>
+#include <string>
+
+int main() {
+    // Fun with C++ output mystery!
+    
+    // Declare variables
+    int x = 5;
+    double y = 2.5;
+    char ch = 'A';
+    std::string message = "Hello, C++!";
+
+    // Output statements
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+    std::cout << "ch: " << ch << std::endl;
+    std::cout << "message: " << message << std::endl;
+
+    return 0;
+}
+
+
+
+Question 1.2.2: Temperature Converter
+
+Prompt the user to enter a temperature in Celsius, and then use a C++ program named "CelsiusToFahrenheitConverter.cpp" to convert the entered temperature to Fahrenheit. 
+Ensure to include clear prompts, handle both uppercase and lowercase input, and add comments to describe each section of the program. 
+Predict the output when a temperature in Celsius is provided.
+
+
+
+Question 1.2.3: Mood-Boosting Chatbox
+
+You're crafting a straightforward chatbot program in C++.
+Request the user to input their current mood using a single-character code ('S' for sad). 
+No matter what the user writes for the input, the program should output a message specifically designed to uplift the user's mood with their input included.
+*/
