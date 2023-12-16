c9338576099d96d23b8d8d48871ba7d4b73d1912
Ayush Nair <141200865+AyushVN10@users.noreply.github.com>
2023-12-16 14:06:39 -0500

    Create Nested Conditional Statements Worksheet Questions.cpp

---
+++ C++ Exercise Questions/Nested Conditional Statements Worksheet Questions.cpp
@@ -0,0 +1,49 @@
+/*
+Question 2.2.1: Ticket Pricing Wonderland
+
+You're designing a ticket pricing system for an amusement park based on age and ride preferences. Implement a nested conditional statement to determine the ticket price:
+
+If the age is 12 or below,
+ - Check if the child prefers roller coasters.
+   - If yes, display "Child Roller Coaster Enthusiast - $10."
+   - If no, display "Child, but not into roller coasters - $8."
+If the age is 15, display "Teen Ticket - $12."
+If the age is 18 or above,
+ - Check if the person is a thrill-seeker.
+   - If yes, display "Adult Thrill-Seeker - $15."
+   - If no, display "Regular Adult - $10."
+
+
+
+Question 2.2.2: Gadget Shipping Validator
+
+You're developing a program called "Gadget Shipping Validator" for an electronics store. 
+The application prompts users to input details about a gadget they want to ship, including its weight in kilograms and dimensions in cubic centimeters. 
+Implement the following conditions:
+
+- If the gadget's weight is over 15 kilograms, inform the user that the gadget is too heavy for standard shipping.
+- If the dimensions exceed 50,000 cubic centimeters, notify the user that the gadget is too large for standard shipping.
+- If both conditions are met, display a message indicating that the gadget is both too heavy and too large for standard shipping.
+- If the gadget meets neither condition, reassure the user that their gadget is eligible for standard shipping.
+- This question challenges your ability to apply nested conditional statements to evaluate diverse criteria and provide tailored feedback based on user inputs.
+
+
+
+Question 2.2.3: Library Book Recommender
+
+You are developing a program for a library that recommends books to users based on their reading preferences. 
+Prompt the user for information on their preferred genre and whether they enjoy fiction or non-fiction. 
+Implement the following conditions using nested conditional statements:
+
+If the user prefers "Mystery," inquire if they like fiction.
+  - If they enjoy fiction, recommend a popular mystery novel.
+  - If they prefer non-fiction, suggest a true crime or investigative journalism book.
+  
+If the user leans toward "Science Fiction," ask if they have a preference for fiction.
+  - If they enjoy fiction, recommend a captivating sci-fi or fantasy novel.
+  - If they prefer non-fiction, suggest a science-related or mythology exploration book.
+  
+If the user selects "Historical," inquire about their interest in fiction.
+  - If they enjoy fiction, recommend a well-researched historical novel.
+  - If they prefer non-fiction, suggest a biography or historical documentation. 
+*/
